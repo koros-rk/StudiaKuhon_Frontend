540d4f72133d4eb9722f5ce439e0d03ed4c6afb8
Sanches-23 <pasechnik23.2002@gmail.com>
2023-04-05 12:07:48 +0300

    s5 - подобається с:

--- src/components/home/ReviewSlider.js
+++ src/components/home/ReviewSlider.js
@@ -1,27 +1,10 @@
 import React from 'react';
 import Slider from "react-slick";
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
-import {all} from "axios";
-
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 export default class ReviewSlider extends React.Component {
     render() {
-        //     const settings = {
-        //         // adaptiveHeight: true,
-        //         // arrows: false,
-        //         dots: true,
-        //         infinite: true,
-        //         speed: 300,
-        //         slidesToShow: 3,
-        //         centerMode: true,
-        //         variableWidth: true,
-        // }
         const settings = {
             dots: true,
-            // slidesToShow: all(), /*ААА, чому я зразу до цього не дійшов...*/
             slidesToShow: 3,
             slidesToScroll: 1,
             infinite: true,
@@ -31,95 +14,17 @@ export default class ReviewSlider extends React.Component {
             arrows: false,
             variableWidth: true,
             // autoplay: true,
-            // autoplaySpeed: 3000,
+            // autoplaySpeed: 3000, надьо?
         }
         return (
-            // <section className="review">
-            //     <div className="review__container">
-            //         <h2 className="review__title main-title light-text">Відгуки наших клієнтів</h2>
-            //     </div>
-            //     <div style={{ transform: 'translate(0%, 0%)' }} className="variable-width dots_slick"> {/*AAAAAAAAAAAAAAA*/}
-            //         <Slider>
-            //             <div className="review__wrapper-slider-item slider-item-1"> {/*style={{ width: '16159px', opacity: '1', transform: 'translate3d(-1243px, 0px, 0px)'}}*/}
-            //                 <p>
-            //                     Сміливо можу рекомендувати цю фірму. У мене був проект під ключ. Ніколи
-            //                     б не подумав раніше, що можна просто розказати дизайнеру, що ти хочеш бачити і через
-            //                     кілька місяців в’їхати у дім мрії. Велике дякую за Вашу роботу!
-            //                 </p>
-            //                 <div>
-            //                     <p>Олександр</p>
-            //                     <p>Хмельницкий</p>
-            //                 </div>
-            //             </div>
-            //             <div className="review__wrapper-slider-item slider-item-2">
-            //                 <p>
-            //                     Дякую за чудовий проект. все чітко і по плану.
-            //                     Квартиру не впізнати. Все зроблено саме так, як я хотіла.
-            //                 </p>
-            //                 <div>
-            //                     <p>Ольга</p>
-            //                     <p>Красилів</p>
-            //                 </div>
-            //             </div>
-            //             <div className="review__wrapper-slider-item slider-item-3">
-            //                 <p>
-            //                     Велике дякую за виконану роботу! Отримала саме той результат, який був необхідний.
-            //                     Продумано все до деталів, креативно і просто, дуже зручно і функціонально. Думаю, що
-            //                     звернусь ще не раз.
-            //                 </p>
-            //                 <div>
-            //                     <p>Наталья</p>
-            //                     <p>Хмельницкий</p>
-            //                 </div>
-            //             </div>
-            //             <div className="review__wrapper-slider-item slider-item-1">
-            //                 <p>
-            //                     Сміливо можу рекомендувати цю фірму. У мене був проект під ключ. Ніколи
-            //                     б не подумав раніше, що можна просто розказати дизайнеру, що ти хочеш бачити і через
-            //                     кілька місяців в’їхати у дім мрії. Велике дякую за Вашу роботу!
-            //                 </p>
-            //                 <div>
-            //                     <p>Олександр</p>
-            //                     <p>Хмельницкий</p>
-            //                 </div>
-            //             </div>
-            //             <div className="review__wrapper-slider-item slider-item-2">
-            //                 <p>
-            //                     Дякую за чудовий проект. все чітко і по плану.
-            //                     Квартиру не впізнати. Все зроблено саме так, як я хотіла.
-            //                 </p>
-            //                 <div>
-            //                     <p>Ольга</p>
-            //                     <p>Красилів</p>
-            //                 </div>
-            //             </div>
-            //             <div className="review__wrapper-slider-item slider-item-3">
-            //                 <p>
-            //                     Велике дякую за виконану роботу! Отримала саме той результат, який був необхідний.
-            //                     Продумано все до деталів, креативно і просто, дуже зручно і функціонально. Думаю, що
-            //                     звернусь ще не раз.
-            //                 </p>
-            //                 <div>
-            //                     <p>Наталья</p>
-            //                     <p>Хмельницкий</p>
-            //                 </div>
-            //             </div>
-            //
-            //         </Slider>
-            //     </div>
-            //
-            // </section>
-
             <section className="review">
                 <div className="review__container">
                     <h2 className="review__title main-title light-text">Відгуки наших клієнтів</h2>
                 </div>
-                .                    {/*</Slider>*/}
-
-
                 <div className="variable-width dots_slick">
-                <Slider {...settings}>
-                         <div className="review__wrapper-slider-item slider-item-1"> {/*style={{margin: '0px', padding: '0px'}}*/}
+                    <Slider {...settings}>
+                        <div
+                            className="review__wrapper-slider-item slider-item-1"> {/*style={{margin: '0px', padding: '0px'}}*/}
                             <p>
                                 Сміливо можу рекомендувати цю фірму. У мене був проект під ключ. Ніколи
                                 б не подумав раніше, що можна просто розказати дизайнеру, що ти хочеш бачити і через
@@ -183,9 +88,8 @@ export default class ReviewSlider extends React.Component {
                                 <p style={{transform: 'translate(0%, 200%)'}}>Хмельницкий</p>
                             </div>
                         </div>
-                </Slider>
+                    </Slider>
                 </div>
-
             </section>
         );
     }
